6dc5bf6e3333e9f5ea54c5e4856f426ad75d200d
Samdatsai <11155012@mail.hpsh.tp.edu.tw>
2025-02-18 15:55:32 +0800

    first commit

---
+++ firstproject/Form1.cs
@@ -0,0 +1,20 @@
+namespace firstproject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "«ö§Ú·F¹À";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
